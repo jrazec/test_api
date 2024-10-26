8db42b1311cd706acc2b41cdb517ad9f92629d1b
jrazec <johnrazeagno@gmail.com>
2024-10-26 11:28:02 +0800

    Add Calendar.js,QRCodeGenerator/Scanner.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Calendar from './Calendar';
+import QRScanner from './QRScanner';
+import QRCodeGenerator from './QRCodeGenerator';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>My Calendar</h1>
+            {/* <Calendar /> */}
+            <QRCodeGenerator />
+        </div>
+    );
 }
 
 export default App;
